a5f377a666f80f694ab3c20754b7d56f08c81317
Héricles Emanuel <hericles.me@icloud.com>
2024-01-20 20:22:33 -0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Héricles Emanuel on 20/01/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

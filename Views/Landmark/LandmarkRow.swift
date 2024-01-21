9fbaed8c8da9055a2fb6703bb1b555588630c8a2
Héricles Emanuel <hericles.me@icloud.com>
2024-01-21 12:07:38 -0300

    feat: landmarks list to details visualization

---
+++ Views/Landmark/LandmarkRow.swift
@@ -0,0 +1,37 @@
+//
+//  LandmarkRow.swift
+//  Landmarks
+//
+//  Created by Héricles Emanuel on 20/01/24.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var landmark: Landmark
+    var body: some View {
+        HStack {
+            landmark.image
+                .resizable()
+                .frame(width: 50, height: 50)
+                .clipShape(Circle())
+                .padding(.trailing, 8.0)
+            Text(landmark.name)
+            
+            Spacer()
+            
+            if landmark.isFavorite {
+                Image(systemName: "star.fill")
+                    .foregroundStyle(.yellow)
+            }
+        }
+    }
+}
+
+#Preview {
+    let landmarks = ModelData().landmarks
+    return Group {
+        LandmarkRow(landmark: landmarks[0])
+        LandmarkRow(landmark: landmarks[1])
+    }
+}
